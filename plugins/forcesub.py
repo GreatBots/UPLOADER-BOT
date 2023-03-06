6d1e43012aa0ecef9bfb618cd2e4eae2c751b1bc
Vivek Tp <Vivektvp1@gmail.com>
2023-03-06 15:47:51 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -24,10 +24,10 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"please Please Join My Updates Channel To Use Me! And click on to Check /start !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("Join Channel", url=link)]
+                        [InlineKeyboardButton("Join Updates Channel", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
